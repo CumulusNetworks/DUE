8bfcf2fbc7bde6210bdbd80006b7e48fa1152a39
Alex Doyle <alexddoyle@gmail.com>
2022-02-07 18:49:40 -0800

    container-create-user.sh - Tabs to spaces.
    Formatting clean up.
    
    Signed-off-by: Alex Doyle <adoyle@nvidia.com>

--- templates/common-templates/filesystem/usr/local/bin/container-create-user.sh
+++ templates/common-templates/filesystem/usr/local/bin/container-create-user.sh
@@ -181,10 +181,10 @@ function fxnAddUserInContainer()
             # Don't create home directory - we presume it is getting mounted
             # use --gecos "" to supply blank data for Full Name, Room number, etc
             if [ "$OS_TYPE" = "RedHat" ];then
-				if [ ! -d /home/"$USER_NAME" ];then
-					# useradd throws warnings if this already exists
-					makeHomeDir=" --home-dir /home/ $USER_NAME" 
-				fi
+                if [ ! -d /home/"$USER_NAME" ];then
+                    # useradd throws warnings if this already exists
+                    makeHomeDir=" --home-dir /home/ $USER_NAME" 
+                fi
                 useradd $makeHomeDir \
                         --gid "$GROUP_ID" \
                         --shell /bin/bash \
@@ -276,8 +276,8 @@ function fxnRunAsUser()
         echo ""
 
         if [ "$OS_TYPE" = "RedHat" ];then
-			# Login behaves differently here, but su and cd _seem_ to be equivalent...
-			cd "$(sudo -u $USER_NAME sh -c 'echo $HOME')"
+            # Login behaves differently here, but su and cd _seem_ to be equivalent...
+            cd "$(sudo -u $USER_NAME sh -c 'echo $HOME')"
             su "${USER_NAME}"
 
         else
@@ -367,14 +367,14 @@ fi
 # If an image-specific /etc/hosts file was present for creaton,
 # append its entries at run time. Docker will replace the /etc/hosts
 # at run time, so this re-applies what the author intended to be there.
-# Typical use would be to handle hostname resolution issues for		
+# Typical use would be to handle hostname resolution issues for     
 #  the container
 if [ -e /due-configuration/filesystem/etc/hosts ];then
-	# If the changes are not there yet, append them
-	diff /due-configuration/filesystem/etc/hosts /etc/hosts \
-		| grep '>' > /dev/null  && { 
-		sudo bash -c "cat /due-configuration/filesystem/etc/hosts >> /etc/hosts"
-	}
+    # If the changes are not there yet, append them
+    diff /due-configuration/filesystem/etc/hosts /etc/hosts \
+        | grep '>' > /dev/null  && { 
+        sudo bash -c "cat /due-configuration/filesystem/etc/hosts >> /etc/hosts"
+    }
 fi
 
 # at the user to the container
